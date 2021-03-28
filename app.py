391baa46f56b9e58213c4b1d0bbafcdb7b2b21d4
larrygoyeau <larry.goyeau@gmail.com>
2021-03-28 22:29:52 +0200

    c

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ def upload_file():
     else:
         return '''
         <!doctype html>
-        <title>Upload new Fileeeeee</title>
+        <title>Upload new Fillllllle</title>
         <h1>Upload new File</h1>
         <form method=post enctype=multipart/form-data>
             <input type=file name=file>
