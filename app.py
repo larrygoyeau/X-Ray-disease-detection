83b0e5c65149ba81f6fa9d2d55fa2bac12e24a9c
larrygoyeau <52501684+larrygoyeau@users.noreply.github.com>
2021-03-29 21:26:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            '''
+            
             im = cv2.imread(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
             im = cv2.resize(im,(700,700))
@@ -80,7 +80,7 @@ def upload_file():
             )
             v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
             cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), v.get_image()[:, :, ::-1])
-            '''
+            
             return redirect(url_for('uploaded_file',
                                     filename=filename))
     else:
