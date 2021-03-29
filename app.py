53a42049d49ec82ec409f759be4115b265647c71
larrygoyeau <larry.goyeau@gmail.com>
2021-03-29 18:06:02 +0200

    c

--- app.py
+++ app.py
@@ -100,4 +100,4 @@ def uploaded_file(filename):
                                filename)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8081)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
