22bb3343be3b2a46a237de27ad716d6c2ea7c229
larrygoyeau <52501684+larrygoyeau@users.noreply.github.com>
2022-10-02 20:03:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -108,5 +108,5 @@ def post():
     return redirect(url_for('upload'))
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 8080))
     app.run(debug=True,host='0.0.0.0',port=port)
